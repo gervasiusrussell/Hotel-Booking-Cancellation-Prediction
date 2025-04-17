447e24f9b84830714714c0fc69977e4ca9a1c1e6
gervasiusrussell <139112604+gervasiusrussell@users.noreply.github.com>
2025-04-17 16:28:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+subprocess.check_call([sys.executable, "-m", "pip", "install", "xgboost"])
 from xgboost import XGBClassifier
 
 # Set page config
